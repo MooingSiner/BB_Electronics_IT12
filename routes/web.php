835418b08f296f41730cb711343f14282f111e2b
MooingSiner <j.damasco.557026@umindanao.edu.ph>
2026-09-24 16:10:18 +0800

    Redirect the root URL to login/dashboard instead of the Laravel splash page
    Visiting / showed the generic Laravel welcome page. Redirect guests
    straight to login and authenticated users to their dashboard instead.

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(auth()->check() ? route('dashboard') : route('login'));
 });
 
 Route::get('/dashboard', function () {
